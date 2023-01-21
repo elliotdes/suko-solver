5ee5c6f5afca25eec9ee2646ade01ab82397724e
elliotdes <elliotdesimons@gmail.com>
2023-01-21 16:17:34 +0000

    Updates

---
+++ precache-manifest.cb86ab0e437d90d7fb7697da076d9cba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8aa2833392a33fc9a767",
+    "url": "/suko-solver/static/css/main.58a88d0a.chunk.css"
+  },
+  {
+    "revision": "8aa2833392a33fc9a767",
+    "url": "/suko-solver/static/js/main.0e04e386.chunk.js"
+  },
+  {
+    "revision": "6817442db8f370884db8",
+    "url": "/suko-solver/static/js/runtime~main.3c9f5323.js"
+  },
+  {
+    "revision": "295f4278c946d775177c",
+    "url": "/suko-solver/static/js/2.954b07a3.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/suko-solver/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "c2f1cba7cd84566c86418a92e87c9a36",
+    "url": "/suko-solver/index.html"
+  }
+];
